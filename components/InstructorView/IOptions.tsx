fab6955fbb96c65c82520736ed82039f70d12b87
ofek168 <marom.ofek@gmail.com>
2022-05-04 14:30:05 -0700

    Under instructorview, updated alignment

--- components/InstructorView/IOptions.tsx
+++ components/InstructorView/IOptions.tsx
@@ -18,36 +18,36 @@ const IOptions = () => {
     const router = useRouter()
     return (
 
-        <div id={styles.I_container}>
+        <div id={styles.I_container}> 
 
            
 
 
             <a className={styles.box}>
                 <h2 className={styles.font}>Your Courses</h2>
+
                 <div className={styles.showCourses}>
                     Show scheduled courses
                     <div></div>
                 </div>
-
                 <div className={styles.coursesBox}>
                     <div className={styles.coursesContent}>
-                    <InstructorCourse courses={[coursesample]}></InstructorCourse>
+                        <InstructorCourse courses={[coursesample]}></InstructorCourse>
                         {/* <p>SPAN 1 - Intro to Spanish A01</p> */}
                         <p>Enrolled: 20/20</p>
                         <p>Fall 2023</p>
-
-
                         <div className={styles.courseEdit}>
                             <button id={styles.btn}>View</button>
                             <button id={styles.btn}>Edit</button>
                             <button id={styles.btn}>Export</button>
                         </div>
-                        <div className={styles.courseCollection}>
-                            <ViewCollection collections={[sample]}></ViewCollection>
-                        </div>
+                            <div className={styles.outer}>
+                                <div className={styles.inner}>
+                                   <ViewCollection collections={[sample]}></ViewCollection>
+                                </div>
+                            </div>
                     </div>
-
+  
                     <div className={styles.coursesContent}>
                     <InstructorCourse courses={[coursesample2]}></InstructorCourse>
                         <p>Enrolled: 20/20</p>
@@ -58,32 +58,30 @@ const IOptions = () => {
                             <button id={styles.btn}>Edit</button>
                             <button id={styles.btn}>Export</button>
                         </div>
-                        <div className={styles.courseCollection}>
-                            <ViewCollection collections={[sample2]}></ViewCollection>
+                        <div className={styles.outer}>
+                            <div className={styles.inner}>
+                               <ViewCollection collections={[sample2]}></ViewCollection> 
+                            </div>
                         </div>
                     </div>
 
-                    {/* <div className={styles.coursesContent}>
-                    <InstructorCourse courses={[coursesample]}></InstructorCourse>
-                        <p>Enrolled: 20/20</p>
-                        <p>Fall'84</p>
-                        <div className={styles.courseEdit}>
-                            <button id={styles.btn}>View</button>
-                            <button id={styles.btn}>Edit</button>
-                            <button id={styles.btn}>Export</button>
-                        </div>
-                        <div className={styles.courseCollection}>
-                            
-                        </div>
-                    </div> */}
                 </div>
+           
+           
+           
+           
+           
             </a>
             <div className={styles.box1}>
                 <h2 className={styles.fonts}>Tools</h2>
 
                 <br></br>
-                <div id={styles.box2}>
-                    <h1 className={styles.font2}>Classes</h1>
+                <div id={styles.box2} >
+                    <div>
+                        <h1 className= {styles.font2}>Classes</h1>
+                    </div>
+    
+
                     <div id={styles.box4}>
                         <Menu />
                     </div>
