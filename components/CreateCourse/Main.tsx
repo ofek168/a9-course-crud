482012b1528c6bab6ea1682327ec66ca6033cfde
RyanPlace <91350027+RyanPlace@users.noreply.github.com>
2022-05-31 19:23:24 -0700

    Update Main.tsx

--- components/CreateCourse/Main.tsx
+++ components/CreateCourse/Main.tsx
@@ -1,18 +1,35 @@
-import React, { FC, InputHTMLAttributes } from 'react';
+import React, { useState, FC, InputHTMLAttributes } from 'react';
 import styles from './createcourse.module.scss'
 import Link from 'next/link'
 import { sample, sample2, coursesample } from '@components/Samples'
-import { ICollection } from 'interfaces'
+import { ICollection,ICourse } from 'interfaces'
 import CollectionsList from '@components/CreatorView/CollectionsView/CollectionsList';
 import router, { useRouter } from 'next/router'
-import ViewCollection from '@components/CreatorView/ViewCollection';
+import {ViewCollectionWithDelete,AddCollections,ViewTemplates} from '@components/CreatorView/ViewCollection';
 import InstructorCourse from '@components/InstructorView/Icourses';
+import Axios from 'axios';
+import ClickAwayListener from 'react-click-away-listener';
+
+const PORT = '4000';
+const HOST = 'localhost'; // 'DOCKER_NODE_SERVICE'; // 'localhost'
+const PUT_END_POINT = 'submitCourse'; //this code requires two endpoints, one for a get request and one for a push
+const GET_END_POINT = 'getCollections';
+const GET_TEMPLATE_END_POINT = 'getTemplates';
+const PUT_TEMPLATE_END_POINT = 'submitTemplate';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     name: string;
     label: string;
 }
+
 const Main: FC<InputProps> = ({ name, label, ...rest }) => {
+    const [buttonPopup, setButtonPopup] = useState(false);
+    const [buttonPopupTemplate, setButtonPopupTemplate] = useState(false);
+    const [currentCollections,setCurrentCollections] = useState<ICollection[]>([]);
+    const [serverCollections,setServerCollections] = useState<ICollection[]>([]);
+    const [selectedCollections,setSelectedCollections] = useState<ICollection[]>([]);
+    const [templates, setTemplates] = useState<ICourse[]>([]);
+
     return (
         <main className = {styles.container}>
             <h1 className = {styles.title} >Create New Course</h1>
@@ -63,23 +80,106 @@ const Main: FC<InputProps> = ({ name, label, ...rest }) => {
             </div>
             <h1 className = {styles.title}>My Collection</h1>
             <div>
-                <ViewCollection collections={[sample, sample2]}></ViewCollection>
+                <ViewCollectionWithDelete collections={currentCollections} updateState={setCurrentCollections}></ViewCollectionWithDelete>
+
                 <button type="button" className={styles.button} onClick={() => {
-                    router.push('/')
+                    Axios.get(`http://${HOST}:${PORT}/${GET_END_POINT}`).then( function (response){
+                        console.log(response.data);
+                        setServerCollections(response.data);
+                        setSelectedCollections([]);
+                        setButtonPopup(true);
+                    });
                 }
                 }>
-                    Select New Collection
+                    Change Collections
                 </button>
+
+                {buttonPopup && (
+                    <ClickAwayListener onClickAway={() => setButtonPopup(false)}>
+                        <div className={styles.modal}>
+                            <div className={styles.modal_content}>
+                            { 
+                                <AddCollections serverCollections={serverCollections} serverUpdateState={setServerCollections} 
+                                selectStates={selectedCollections} selectUpdateStates= {setSelectedCollections}/>
+                                
+                            }
+                            <button type="button" className={styles.button} onClick={() => {
+                            //change collection state here
+                            setCurrentCollections(currentCollections.concat(selectedCollections));
+                            setSelectedCollections([]);
+                            setButtonPopup(false);
+                                }
+                                }>
+                                    Submit
+                            </button>
+
+                            <button type="button" className={styles.button} onClick={() => {
+                                setSelectedCollections([]);
+                                setButtonPopup(false);
+                                }
+                                }>
+                                    Cancel
+                            </button>
+                            </div>
+                        </div>
+                        
+                    </ClickAwayListener>
+                    
+                )}
+                <button type="button" className={styles.button} onClick={() => {
+                    Axios.get(`http://${HOST}:${PORT}/${GET_TEMPLATE_END_POINT}`).then(function (response){
+                        console.log(response.data);
+                        setTemplates(response.data);
+                        setButtonPopupTemplate(true);
+    
+                    });
+                }
+                }>
+                    Start From Template
+                </button>
+                <button type="button" className={styles.button} onClick={() => {
+                    Axios.put(`http://${HOST}:${PORT}/${PUT_TEMPLATE_END_POINT}`,currentCollections);
+                }
+                }>
+                    Submit Template
+                </button>
+
+                {buttonPopupTemplate && (
+                    <ClickAwayListener onClickAway={() => setButtonPopupTemplate(false)}>
+                        <div className={styles.modal}>
+                            <div className={styles.modal_content}>
+                                { <ViewTemplates templates={templates} currentCollections={currentCollections} setCurrentCollections={setCurrentCollections} setButtonPopupTemplate= {setButtonPopupTemplate}/>}
+                                <button type="button" className={styles.button} onClick={() => {
+                                //change collection state here
+                                setCurrentCollections(selectedCollections);
+                                setSelectedCollections([]);
+                                setButtonPopupTemplate(false);
+                                    }
+                                    }>
+                                        Cancel
+                                </button>
+                            </div>
+                        </div>
+                        
+                    </ClickAwayListener>
+                    
+                )}
+
                 <button type="button" className={styles.button} onClick={() => {
                     router.push('/instructor_view')
                 }
                 }>
                     Back
                 </button>
-
+                <button type="button" className={styles.button} onClick={() => {
+                    Axios.put(`http://${HOST}:${PORT}/${PUT_END_POINT}`,currentCollections);
+                }
+                }>
+                    Submit
+                </button>
             </div>
             <div className = {styles.instructorCourse}>
-            <InstructorCourse courses={[coursesample]}></InstructorCourse>
+                <InstructorCourse courses={[coursesample]}></InstructorCourse>
             </div>
         </main>
 
